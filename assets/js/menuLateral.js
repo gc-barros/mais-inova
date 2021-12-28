c28b933cfe78fcde95c57ae11c182e9a553e31fe
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-12-28 12:26:32 -0300

    Add animação no menu lateral

--- assets/js/menuLateral.js
+++ assets/js/menuLateral.js
@@ -3,23 +3,32 @@ function menuLateralInit() {
     const hamburguer = document.querySelector("[data-hamburguer]");
     const closeButton = document.querySelector("[data-close-menu]");
     const overlay = document.querySelector("[data-overlay]");
-
+    
     hamburguer.addEventListener("click", () => {
         menu.classList.add("cabecalho__menu--ativo");
         overlay.classList.add("overlay-ativo");
     });
-
+    
     closeButton.addEventListener("click", () => {
         fecharMenuLateral();
     });
-
+    
     overlay.addEventListener('click', () => {
-        fecharMenuLateral();
+        const menuAtivo = menu.classList.contains("cabecalho__menu--ativo");
+        if (menuAtivo) {
+            fecharMenuLateral();
+        }
     })
 
     function fecharMenuLateral() {
-        menu.classList.remove("cabecalho__menu--ativo");
-        overlay.classList.remove("overlay-ativo");
+        menu.classList.add("anima-saida");
+        overlay.classList.add("anima-saida");
+        setTimeout(() => {
+            menu.classList.remove("cabecalho__menu--ativo");
+            menu.classList.remove("anima-saida");
+            overlay.classList.remove("overlay-ativo");
+            overlay.classList.remove("anima-saida");
+        }, 400);
     }
 }
 
