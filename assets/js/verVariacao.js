20686befa60f459230e1115a384ba195ce5bc245
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2022-01-07 18:53:49 -0300

    Adicionando função "ver variação" e gráficos

--- assets/js/verVariacao.js
+++ assets/js/verVariacao.js
@@ -1,5 +1,34 @@
 function verVariacaoInit() {
+    const botoesVariacao = document.querySelectorAll("[data-vervariacao]");
+    const overlay = document.querySelector("[data-overlay]");
+    const modal = document.querySelector("[data-modal]");
+    const botaoFechar = document.querySelector("[data-fechar-modal]");
 
+    botoesVariacao.forEach((botao) => {
+        botao.addEventListener("click", () => {
+            modal.classList.add("modal-ativa");
+            overlay.classList.add("overlay-ativo")
+        })
+    })
+
+    botaoFechar.addEventListener('click', () => {
+        fecharModal();
+    })
+
+    overlay.addEventListener("click", () => {
+        fecharModal();
+    })
+
+    function fecharModal() {
+        modal.classList.add("anima-saida");
+        overlay.classList.add("anima-saida");
+        setTimeout(() => {
+            modal.classList.remove("modal-ativa");
+            modal.classList.remove("anima-saida");
+            overlay.classList.remove("overlay-ativo");
+            overlay.classList.remove("anima-saida");
+        }, 400);
+    }
 }
 
 export default verVariacaoInit;
