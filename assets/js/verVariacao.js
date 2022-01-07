9f1d8b27f395af6fbd8d4678df52939ad84aac6b
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2022-01-07 20:42:34 -0300

    Sincronizando gráficos e ativos

--- assets/js/verVariacao.js
+++ assets/js/verVariacao.js
@@ -3,9 +3,14 @@ function verVariacaoInit() {
     const overlay = document.querySelector("[data-overlay]");
     const modal = document.querySelector("[data-modal]");
     const botaoFechar = document.querySelector("[data-fechar-modal]");
+    const grafico = document.querySelector("[data-grafico]");
 
     botoesVariacao.forEach((botao) => {
+        let tipo = capturarTipo(botao);
+        let source = `assets/img/graficos/grafico-${tipo}.png`;
+
         botao.addEventListener("click", () => {
+            grafico.setAttribute("src", source);
             modal.classList.add("modal-ativa");
             overlay.classList.add("overlay-ativo")
         })
@@ -28,6 +33,14 @@ function verVariacaoInit() {
             overlay.classList.remove("overlay-ativo");
             overlay.classList.remove("anima-saida");
         }, 400);
+    }
+
+    function capturarTipo(botao) {
+        let divPai = botao.parentNode;
+        divPai = divPai.parentNode;
+        let tipo = divPai.querySelector(".investimentos__ativo--tipo");
+
+        return tipo.textContent;
     }
 }
 
